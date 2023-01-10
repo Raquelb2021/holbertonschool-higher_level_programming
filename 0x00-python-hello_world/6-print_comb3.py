a970fe0287b1792866d54b1bcaf6ed6e1ced6862
root <raquelmcbezerra@gmail.com>
2023-01-10 16:32:08 +0000

    Write a program that prints all possible different combinations of two digits.

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for x in range(0, 9):
+for x in range(0, 10):
     for a in range(1, 10):
         if x >= a:
             continue
-    elif x == 8 and a == 9:
-        print("{}{}".format(x, a))
-    else:
-        print("{}{}".format(x, a), end=", ")
+        elif x == 8 and a == 9:
+            print("{}{}".format(x, a))
+        else:
+            print("{}{}".format(x, a), end=", ")
