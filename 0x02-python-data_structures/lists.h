aea0925eb92d6c0c7e6ef60d46623e002361976c
root <raquelmcbezerra@gmail.com>
2023-01-13 03:49:02 +0000

    function that prints all integers of a list, in reverse order.

--- 0x02-python-data_structures/lists.h
+++ 0x02-python-data_structures/lists.h
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+def print_list_integer(my_list=[]):
 def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
 
 #endif LISTS_H
