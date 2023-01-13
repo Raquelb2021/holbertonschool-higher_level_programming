c655fc88f402c80093bcff4640fd95da4e71b79a
root <raquelmcbezerra@gmail.com>
2023-01-13 19:38:54 +0000

    no-'c' and 'C'

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = list(my_string)
     for i in new_string:
         if i == 'c' or i == 'C':
-           new_string.remove(i)
+            new_string.remove(i)
     return("".join(new_string))
