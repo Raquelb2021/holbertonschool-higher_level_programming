9ff3a4fff0bd62e78d2c15b21d907437d7ddff47
root <raquelmcbezerra@gmail.com>
2023-01-13 19:53:29 +0000

    no c

--- 0x02-python-data_structures/lists.h
+++ 0x02-python-data_structures/lists.h
@@ -5,10 +5,5 @@
 #include <stdio.h>
 #include <unistd.h>
 
-def print_list_integer(my_list=[]):
-def element_at(my_list, idx):
-def replace_in_list(my_list, idx, element):
-def print_reversed_list_integer(my_list=[]):
-def new_in_list(my_list, idx, element):
-def no_c(my_string):
+
 #endif LISTS_H
