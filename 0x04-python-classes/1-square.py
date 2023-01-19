7ebd696204eef54abd29957ae247053dcaee3850
root <raquelmcbezerra@gmail.com>
2023-01-19 02:29:09 +0000

    size must be an integer

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square:
+    """Define a square."""
+
     def __init__(self, size=0):
-        self.__size = size
+        """Private instance attribute: size"""
+
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
