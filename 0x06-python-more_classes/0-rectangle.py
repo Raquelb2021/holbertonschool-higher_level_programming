02ed20da5ee5bc79f4d1dc9c1cb32070eff4854a
root <raquelmcbezerra@gmail.com>
2023-01-20 21:23:06 +0000

    Write a class Rectangle

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
-            raise TypeError('width must be >= 0')
+            raise ValueError('width must be >= 0')
         else:
             self.__width = value
 
@@ -35,6 +35,6 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
-            raise TypeError('height must be >= 0')
+            raise ValueError('height must be >= 0')
         else:
             self.__height = value
