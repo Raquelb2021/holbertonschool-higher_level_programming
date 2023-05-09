6068acdaf7c6f0f08845ff5baf096ddf6cec1996
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-09 13:14:48 -0700

    Print all the letters except q and e in python

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ch in range(ord('a'), ord('z')+1):
-    if ch not in [ord ('q'), ord ('e')]:
+    if ch not in [ord('q'), ord('e')]:
         print("{:c}".format(ch), end='')
