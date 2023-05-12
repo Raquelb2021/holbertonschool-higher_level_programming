6638fcac0a3b49144a3be7151755ea5273529616
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-12 12:08:11 -0700

    list in reverse

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    new_list = [my_list[len(my_list) - i]
+            for i in range(1, len(my_list)+1)]
+    for i in new_list:
+         print("{}".format(i))
