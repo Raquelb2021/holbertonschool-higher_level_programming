f2bbacc6b667dcf627c8adf865cbb7bae5b6e719
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-15 11:24:02 -0700

    replace elements of a list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    search = 2
-    replace = 89
-    new_list = [replace if item == search else item for item in my_list]
+    new_list = [replace if x == search else x for x in my_list]
     return new_list
