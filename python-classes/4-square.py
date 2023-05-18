8c784f917cd7f6f0dda74375fbc7a8a197340c2a
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-18 11:18:19 -0700

    @property and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Initialize the class Square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return (self.__size ** 2)
