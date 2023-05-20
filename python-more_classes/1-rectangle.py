f405597720068c6bda0ccc20c3d46c0bf70a552a
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-20 09:41:49 -0700

    defines a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize the data"""
 
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
@@ -20,7 +20,7 @@ class Rectangle:
     def width(self, value):
         """set the width of the Rectangle"""
         if not isinstance(value, int):
-            raise TypeError("width must be >= 0")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
