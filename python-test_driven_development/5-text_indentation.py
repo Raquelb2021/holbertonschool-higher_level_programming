bb8064f499da2059af9819757755ecc415bac837
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-23 14:30:42 -0700

    4. Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,8 +6,7 @@ def text_indentation(text):
     """prints a text with 2 new lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
-        if char in ['.', '?', ':']:
-            print(char, end='\n\n')
-        else:
-            print(char, end='')
+    for char in ".:?":
+        text = text.replace(char, char + "\n\n")
+        print("\n".join(line.strip()
+            for line in text.split("\n")))
