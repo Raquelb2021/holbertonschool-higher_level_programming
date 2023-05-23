524e56ff9f051253b4e7d282b644c76e4b2c7e48
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-23 11:22:42 -0700

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""This module define a funtion print_square"""
+
 
 def print_square(size):
+    """Function that takes teh argument size as parameter
+    and print a square using the character # """
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for row in range(size):
+        for row in range(size):
             [print("#", end="") for col in range(size)]
             print("")
