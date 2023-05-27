186720295958eb93eac706f894bd028cc6565b74
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-26 17:53:47 -0700

    te a class BaseGeometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Define a class BaseGeometry"""
 
 
 class BaseGeometry:
+    """represent BaseGeometry"""
 
     def area(self):
         raise Exception("area() is not implemented")
@@ -10,4 +12,6 @@ class BaseGeometry:
         self.name = name
         self.value = value
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer") 
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
