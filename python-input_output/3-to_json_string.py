fffc2b13f27ffe2e0ebc0e51e5a327982bb7f557
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-29 12:10:02 -0700

    returns the JSON representation of an object (string):

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+import json
+"""
+Returns th JSON representation of an object as a string
+
+Args:
+    my_obj: The object to be serialized.
+    This can be any Pyhton object,
+    that is JSON serializable,
+    such as a
+    dictionary, list, tuple, string, integr, float, boolean
+    or None.
+
+    Returns:
+    A string containing the JSON representation of the input object.
+    """
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
