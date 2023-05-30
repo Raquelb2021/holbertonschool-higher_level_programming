1d47ae37020d7e401484477b5c044ef2de7645ec
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-29 17:50:21 -0700

    script that adds all arguments to a Python list, and then save them to a file:

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,16 +4,9 @@ adds them to a list, and saves the list as a JSON
 representation in a file named add_item.json:
     """
 import sys
-"""This is a Python script that
-takes command line arguments and adds them to a list
-"""
+
 
 if __name__ == "__main__":
-    """
-    The script first tries to load an existing list
-    from the file add_item.json using the load_from_json_file
-    function. If the file is not found, an empty list is created
-    """
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
