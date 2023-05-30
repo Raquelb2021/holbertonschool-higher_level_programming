a9f18f27639b449c145bea1326170f64917f0a5f
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-05-29 18:25:13 -0700

    returns the dictionary description with simple data structure

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-
-def class_to_json(obj):
-    """
+"""
     Returns the dictionary
     description with simples data
     structure
     for JSON serialization of an object
     """
+def class_to_json(obj):
     return obj.__dict__
