4020fc68b921d40597d66298c715d2a1c063fdb9
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-02 21:40:33 -0700

    base test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,13 +2,23 @@
 
 import unittest
 from models.base import Base
+"""Test for the Base class"""
+
 
 class TestBase(unittest.TestCase):
+    """initialize tests"""
 
-    def test_auto_id(self):
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id + 1, b2.id)
+    def test_no_id(self):
+        """Testing with no id"""
+        b = Base()
+        self.assertEqual(Base._Base__nb_objects, 1)
+
+    def test_with_id(self):
+        """testing with id value"""
+        b = Base(100)
+        self.assertEqual(b.id, 100)
+        self.assertEqual(Base._Base__nb_objects, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
