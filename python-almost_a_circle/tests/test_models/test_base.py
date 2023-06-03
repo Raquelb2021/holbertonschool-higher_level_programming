8c9f7042f8bf4bb96fbb6299252b0a8c7f0cb1f1
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-02 20:29:07 -0700

    almost circle

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,13 +5,10 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
-    def test_id(self):
-        b = Base()
-        self.assertEqual(b.id, 1)
-
-    def test_id_set(self):
-        b = Base(12)
-        self.assertEqual(b.id, 12)
+    def test_auto_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id + 1, b2.id)
 
 if __name__ == '__main__':
     unittest.main()
