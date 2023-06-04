51aeaafe7d9e2ed421633889f7c1c6c3f607ca16
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-03 20:40:29 -0700

    square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherits from Rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Call the super class with id, x, y, width and height
+        The width and height must be assigned to the value of size
+        """
+        super().__init__(id=id, x=x, y=y, width=size, height=size)
+
+    def __str__(self):
+        """__str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
