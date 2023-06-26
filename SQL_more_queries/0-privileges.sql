b37f9916988aaca6bfee3eedc34b7d1f85fcb8b2
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-26 13:07:28 -0700

    0. My privileges;

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,21 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+import mysql.connector
+
+# Connect to the MySQL server
+cnx = mysql.connector.connect(user='root', password='password', host='localhost')
+cursor = cnx.cursor()
+
+# Define the users to check
+users = ['user_0d_1', 'user_0d_2']
+
+# Loop through the users and show their privileges
+for user in users:
+    print(f"Privileges for user {user}:")
+    cursor.execute(f"SHOW GRANTS FOR '{user}'@'localhost'")
+    for row in cursor:
+        print(row[0])
+    print()
+
+# Close the cursor and connection
+cursor.close()
+cnx.close()
+
